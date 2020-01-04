0160232e24221e02317d3ea3c9f47df27ff99894
Pavel Romanov <pavel.romanov101@gmail.com>
2020-01-04 12:53:24 +0200

    Update project.hs

--- project.hs
+++ project.hs
@@ -1,7 +1,10 @@
 import System.IO
 import Control.Monad 
 
-append' lst1 lst2 = lst1 ++ lst2
+areEqual lst1 lst2
+ |lst2 == [] = "Could't find you animal"
+ |lst1 == fst (head lst2) = "Is your animal " ++ snd (head lst2) ++ " ?"
+ |otherwise = areEqual lst1 (tail lst2)
 
 -- Make a list of questions to ask
 questions = ["Are you ready to play", "Is it a mammal? ", "Is it a bird? ", "Does it have 4 legs?", "Does it have 2 legs?", "Is it a carnivore?", "Is it furry?"]
@@ -14,10 +17,21 @@ eagle = ["no", "yes", "no", "yes", "no", "yes"]
 cat = ["yes", "no", "yes", "no", "no", "yes"]
 lion = ["yes", "no", "yes", "no", "no", "yes"]
 
+listAnimals = [(elephant, "elephant") , (shark, "shark"), (eagle, "eagle") , (cat, "cat") , (lion, "lion")]
+
 --User input that is put into a list of answers - Answers1
 main = do   
-    answers: answers1 <- forM questions (\a -> do  
+    answer: answers1 <- forM questions (\a -> do  
         putStrLn $ show a   
         answer <- getLine
         return answer)
-    print answers1
+    putStrLn (areEqual answers1 listAnimals)
+    input <- getLine
+    if input == "Yes" then putStrLn "Great!" else putStrLn "Could't find your animal :("
+    putStrLn "Do you want to play again?"
+    input1 <- getLine
+    if input1 == "Yes" then main else putStrLn "See you next time!"
+
+
+   --if areEqual answers1 listAnimals then print answers1 else putStrLn "Could't guess your animal :("
+
